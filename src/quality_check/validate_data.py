5ea486ccc910a9d07b6af56c32f6c7d627ac4c19
punyapat <punyapat.sw@gmail.com>
2021-11-18 00:29:16 -0800

    add docstring on validate_data.py

--- src/quality_check/validate_data.py
+++ src/quality_check/validate_data.py
@@ -1,13 +1,30 @@
 from pyspark.sql.functions import *
 
-def compute_stats(spark:'spark.Session',tbl_name:str,data_date:str):
+def compute_stats(spark:sparkSession,tbl_name:str,data_date:str):
+    """compute hive table statistics
+
+    Args:
+        spark (SparkSession): spark session
+        tbl_name (str): hive table name
+        data_date (str): logical execution date format YYYY-MM-DD
+    """
     if data_date is not None:
         sql="ANALYZE TABLE {} PARTITION(data_date='{}') COMPUTE STATISTICS".format(tbl_name,data_date)
     else :
         sql="ANALYZE TABLE {} COMPUTE STATISTICS".format(tbl_name)
     spark.sql(sql)
     
-def validate_data(spark:'spark.Session',df:'spark.DataFrame',tbl_name:str,data_date:str,col:str):
+def validate_data(spark:sparkSession,df:sparkDataFrame,tbl_name:str,data_date:str,col:str):
+    """validate data insert completeness by compare result dataframe with data in hive table
+
+    Args:
+        spark (SparkSession): spark session
+        tbl_name (str): hive table name
+        data_date (str): logical execution date format YYYY-MM-DD
+        col (str): partitioned column
+    """
+    
+    
     count_df = df.count()
     sum_df=df.select(sum(col)).collect()[0][0]
     
