35fe90cd0467e6ddc0a38e26bb026baf691bdce1
punyapat <punyapat.sw@gmail.com>
2021-11-23 23:29:59 -0800

    fix typo

--- dags/flow_mf_txrn.py
+++ dags/flow_mf_txrn.py
@@ -1,6 +1,11 @@
+# airflow related
 from airflow import DAG
+# from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.sensors.filesystem import FileSensor
+# from etl_flow import etl_flow
+
+# other packages
 from datetime import datetime
 from datetime import timedelta
 
@@ -15,11 +20,20 @@ default_args = {
     'retry_delay': timedelta(seconds=5),
 }
 
+# def mf_txrn(**context):
+#     etl_flow.etl_flow('mf_txrn', context['execution_date'])
+
 dag = DAG(
   dag_id='flow_mf_txrn', 
   description='Load file mf_txrn',
   default_args=default_args,
   catchup=False)
+
+# task1 = PythonOperator(
+#   task_id='load_mf_txrn', 
+#   python_callable=mf_txrn, 
+#   provide_context=True,
+#   dag=dag)
 
 task1 = BashOperator(
   task_id='load_mf_txrn', 
@@ -29,10 +43,9 @@ task1 = BashOperator(
 watchfile_mf_txrn = FileSensor(
                   task_id='wait_file_mf_txrn',
                   poke_interval=60,
-                  filepath='/opt/airflow/dags/mock_fund_buying_transaction_data.csv',
+                  filepath='/opt/airflow/sources/mock_fund_buying_transaction_data.csv',
                   dag=dag
                   )
 
 # setting dependencies
-# wait for file trigger
 watchfile_mf_txrn >> task1
